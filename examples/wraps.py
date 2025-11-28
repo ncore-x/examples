29690157d00130c99e5e57f6cb41a4e2864d6a2f
ncore-x <ncore.eng@gmail.com>
2025-11-28 23:52:21 +1100

    add wraps func

---
+++ examples/wraps.py
@@ -0,0 +1,18 @@
+from functools import wraps
+from typing import Callable
+
+
+def log(message: str):
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(f"[LOG] {message}")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def hello(name: str):
+    print(f"Привет, {name}!")
+
+
+hello("Ашот")
